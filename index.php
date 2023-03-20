59d9ab89a937dac079538966ca6d6d8d032dde30
Jim Pouwels <jim.pouwels@philips.com>
2023-03-20 09:42:03 +0100

    updated htaccess and dir

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     // DIRECT ACCESS GRANTED
     define("_ACCESS", "GRANTED");
-    define("CMS_ROOT", '/Users/phnl310098470/projects/personal/travel-blog/www/admin/');
+    define("CMS_ROOT", './admin/');
     require_once CMS_ROOT . "database_config.php";
     require_once CMS_ROOT . "constants.php";
     require_once CMS_ROOT . "frontend/handlers/request_handler.php";
