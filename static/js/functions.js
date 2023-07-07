4f3caa3a6752f2043679f0ac892c8ed07853cc83
Jim Pouwels <jim.pouwels@philips.com>
2023-07-07 12:31:25 +0200

    scroll/resize

--- static/js/functions.js
+++ static/js/functions.js
@@ -14,19 +14,45 @@ $(document).ready(function() {
     var el = document.getElementById('right-content');
     el.style.marginTop = "20px";
     var initialPos = $('#right-content').position();
-    var initialWidth = $('#right-content').width();
-    handleScroll(initialWidth, initialPos);
+    var currentWidth = $('#right-content').width();
+    var initialWidth = $('#right-content').width() + 20;
+    console.log('test ' + initialWidth);
+    var windowWidth = $(window).width();
+    var windowWidthDelta = 0;
+    var paddingPx = parseInt($('#right-content').css('padding').replace('px', ''));
+    var paddingLeftPx = parseInt($('#right-content').css('padding-left').replace('px', ''));
+
+    var marginPx = parseInt($('#right-content').css('margin').replace('px', ''));
+    var marginLeftPx = parseInt($('#right-content').css('margin-left').replace('px', ''));
+    console.log('padding: ' + paddingPx);
+    console.log('padding-left: ' + paddingLeftPx);
+    console.log('margin: ' + marginPx);
+    console.log('margin-left: ' + marginLeftPx);
+    // var marginPx = parseInt($('#right-content').css('margin-left').replace('px', ''));
+    var marginLeft = initialPos.left - ($('#page-content').position().left + $('#page-content').width());
+    handleScroll(initialPos, currentWidth, initialPos, false);
+    
     $(window).scroll(function(e) { 
-        handleScroll(initialWidth, initialPos);
+        var currentPos = $('#right-content').position();
+        handleScroll(currentPos, initialPos, false);
     });
 
-    function handleScroll(initialWidth, initialPos) {
-        var scrollPos = $(window).scrollTop();
+    $(window).resize(function() {
+        windowWidthDelta = windowWidth - $(this).width();
+        windowWidth = $(this).width();
+        currentWidth -= (windowWidthDelta * 0.25);
         var currentPos = $('#right-content').position();
+        handleScroll(currentPos, initialPos, true);
+    });
+
+    function handleScroll(currentPos, initialPos, resize) {
+        var scrollPos = $(window).scrollTop();
         if (scrollPos > 585) {
-            $('#right-content').css({width: initialWidth + 'px', position: 'fixed', top: 65 + 'px', left: currentPos.left + 'px', currentPos: initialPos.bottom + 'px'});
+            var newLeft = !resize ? (currentPos.left) : (($('#page-content').position().left + $('#page-content').width()) + marginLeft);
+            $('#right-content').css({width: (currentWidth - windowWidthDelta) + 'px', marginLeft: marginLeftPx + 'px', padding: paddingPx + 'px', position: 'fixed', top: 65 + 'px', left: newLeft + 'px', bottom: initialPos.bottom + 'px'});
         } else {
             $('#right-content').css({position: 'static'});
         }
+        windowWidthDelta = 0;
     }
 });
