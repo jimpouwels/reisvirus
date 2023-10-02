483c5fc4a1ed93b8bddc67e18e70fc150d9c985c
Jim Pouwels <jim.pouwels@philips.com>
2023-10-02 08:59:46 +0200

    setting CMS ROOT the right way

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-    // DIRECT ACCESS GRANTED
-    define("_ACCESS", "GRANTED");
-    define("CMS_ROOT", './admin/');
-    require_once CMS_ROOT . "database_config.php";
-    require_once CMS_ROOT . "constants.php";
-    require_once CMS_ROOT . "frontend/handlers/request_handler.php";
+// DIRECT ACCESS GRANTED
+define("_ACCESS", "GRANTED");
+define("CMS_ROOT", dirname(__FILE__) . '/admin');
+require_once CMS_ROOT . "/database_config.php";
+require_once CMS_ROOT . "/constants.php";
+require_once CMS_ROOT . "/frontend/handlers/request_handler.php";
 
-    $request_handler = new RequestHandler();
-    $request_handler->handleRequest();
-?>
+$request_handler = new RequestHandler();
+$request_handler->handleRequest();
