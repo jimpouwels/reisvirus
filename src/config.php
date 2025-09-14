1b71a1642acba495550cde2f9fc5c759f04bd144
Jim Pouwels <jim.pouwels@philips.com>
2025-09-14 12:45:12 +0200

    styles

--- src/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-const PRIVATE_DIR_LOCAL = "/../private";
-const PRIVATE_DIR_PRODUCTION = "/../httpd.private";
--- src/index.php
+++ src/index.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace reisvirus\reisvirus;
+
+define("PRIVATE_DIR_LOCAL", "/../private");
+define("PRIVATE_DIR_PRODUCTION", "/../httpd.private");
+
 require_once "bootstrap.php";
 
 \Obcato\render();
